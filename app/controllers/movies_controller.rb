155a586b055ddfb4091a5b64aa64aed420b363ec
Tyler Davis <tyebud@gmail.com>
2013-12-02 10:40:15 -0800

    Updated index for expiring films

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -5,7 +5,7 @@ class MoviesController < ApplicationController
   # GET /movies
   # GET /movies.json
   def index
-    @movies = Movie.order(sort_column + " " + sort_direction + " NULLS LAST")
+    @movies = Movie.where("expire >= ?", Time.now).order(sort_column + " " + sort_direction + " NULLS LAST")
   end
 
   # GET /movies/1
