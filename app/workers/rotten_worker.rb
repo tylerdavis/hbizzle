d69961c8699c2af544693e51a5e86bcbcbf34b59
Tyler Davis <tyebud@gmail.com>
2013-12-04 08:12:02 -0800

    updated worker for edge case response

--- app/workers/rotten_worker.rb
+++ app/workers/rotten_worker.rb
@@ -4,8 +4,8 @@ class RottenWorker
   def perform(id)
     @movie = Movie.find(id)
     @rotten_movie = RottenMovie.find(:title => @movie.title, :limit => 1)
-    @movie.rotten_critics_score = @rotten_movie.ratings.critics_score
-    @movie.rotten_audience_score = @rotten_movie.ratings.audience_score
+    @movie.rotten_critics_score = @rotten_movie.first.ratings.critics_score
+    @movie.rotten_audience_score = @rotten_movie.first.ratings.audience_score
     @movie.save
   end
 end
