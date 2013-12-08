16e5c7016c66d89d290683f1459f3a4708715d94
Tyler Davis <tyebud@gmail.com>
2013-12-08 00:04:29 -0800

    Added a cookie that tracks plays from the client

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,6 +12,7 @@
 //
 //= require jquery
 //= require angular
+//= require angular-cookies
 //= require bootstrap/transition
 //= require bootstrap/modal
 //= require_tree .
--- app/assets/javascripts/controllers/index.js
+++ app/assets/javascripts/controllers/index.js
@@ -1,35 +1,40 @@
-angular.module('controllers.index', ['filters.start_from', 'ui.bootstrap', 'ui.bootstrap.tpls']);
-angular.module('controllers.index').controller('IndexCtrl', ['$http', '$scope', '$location', function ($http, $scope, $location) {
-  
-  $scope.currentPage = 1;
-  $scope.movies = window.movies;
-  $scope.pageSize = 6;
-  $scope.predicate = '-meta_score';
-  $scope.showPagesNum = 8;
+angular.module('controllers.index', ['filters.start_from', 'ngCookies', 'ui.bootstrap', 'ui.bootstrap.tpls']);
+angular.module('controllers.index').controller('IndexCtrl', ['$cookies', '$http', '$scope', '$location', 
+  function ($cookies, $http, $scope, $location) {
 
-  $scope.goToCurrentPage = function () {
     $scope.currentPage = 1;
+    $scope.movies = window.movies;
+    $scope.pageSize = 6;
     $scope.predicate = '-meta_score';
-    $scope.reverse = false;
-  };
+    $scope.showPagesNum = 8;
 
-  $scope.play = function (movie, event) {
-    $http({
-      data : { hbo_id : movie.hbo_id },
-      method : 'POST',
-      url : '/play'
-    });
-  };
+    var cookies = (!!$cookies.hbizzle) ? JSON.parse($cookies.hbizzle) : [];
 
-  $scope.sortDirection = function () {
-    return ($scope.reverse) ? 'ascending' : 'descending';
-  };
-
-  $scope.updatePredicate = function (predicate) {
-    if ($scope.predicate === predicate) {
-      $scope.reverse = !$scope.reverse
-    };
+    $scope.goToCurrentPage = function () {
       $scope.currentPage = 1;
-      $scope.predicate = predicate;
-  };
+      $scope.predicate = '-meta_score';
+      $scope.reverse = false;
+    };
+
+    $scope.play = function (movie, event) {
+      cookies.push(movie.hbo_id);
+      $cookies.hbizzle = JSON.stringify(cookies);
+      $http({
+        data : { hbo_id : movie.hbo_id },
+        method : 'POST',
+        url : '/play'
+      });
+    };
+
+    $scope.sortDirection = function () {
+      return ($scope.reverse) ? 'ascending' : 'descending';
+    };
+
+    $scope.updatePredicate = function (predicate) {
+      if ($scope.predicate === predicate) {
+        $scope.reverse = !$scope.reverse
+      };
+        $scope.currentPage = 1;
+        $scope.predicate = predicate;
+    };
 }]);
