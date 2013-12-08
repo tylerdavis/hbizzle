12ee60006308e890263788acdcd131aebdc5fc93
Tyler Davis <tyebud@gmail.com>
2013-12-07 16:52:22 -0800

    Resolved issue where plays were not being submitted.

--- app/assets/javascripts/controllers/index.js
+++ app/assets/javascripts/controllers/index.js
@@ -7,18 +7,24 @@ angular.module('controllers.index').controller('IndexCtrl', ['$http', '$scope', 
   $scope.pageSize = 6;
   $scope.showPagesNum = 8;
 
+  $scope.goToCurrentPage = function () {
+    $scope.currentPage = 1;
+    $scope.predicate = '-meta_score';
+    $scope.reverse = false;
+  };
+
   $scope.play = function (movie, event) {
-    event.preventDefault();
     var data = { hbo_id : movie.hbo_id };
-    $http.post('/play', data)
-      .success(function () {
-        window.open(movie.hbo_link);
-      });
+    $http({
+      url : '/play',
+      method : 'POST',
+      data : data
+    });
   };
 
   $scope.sortDirection = function () {
     return ($scope.reverse) ? 'ascending' : 'descending';
-  }
+  };
 
   $scope.updatePredicate = function (predicate) {
     if ($scope.predicate === predicate) {
