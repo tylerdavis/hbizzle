1197a9e9e79586bef2e206bce9306a3a4926554d
Tyler Davis <tyebud@gmail.com>
2013-12-18 22:52:31 -0800

    Moved update created rake task to its own file

---
+++ lib/tasks/update_created.rb
@@ -0,0 +1,11 @@
+desc "update created time stamps on existing listings"
+task update_created: :environment do
+  puts "Updating timestamps..."
+  xml = Nokogiri::XML(open(Movie::HBO_XML_URL))
+  hbo_features = Hash.from_xml(xml.to_s)['response']['body']['productResponses']['featureResponse']
+  hbo_features.each do |feature|
+    @movie = Movie.where(title: feature['title']).first
+    @movie.created_at = feature['startDate']
+    @movie.save
+  end
+end
