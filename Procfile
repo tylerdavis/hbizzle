3484bbbc1edb9f8a61bc7a1161d37969d53435ee
Tyler Davis <tyebud@gmail.com>
2013-12-30 11:18:33 -0800

    Un-thread sidekiq

--- Procfile
+++ Procfile
@@ -1,2 +1,2 @@
-web: bundle exec unicorn -p 8080 -c ./config/unicorn.rb -E production
+web: bundle exec unicorn_rails -p 8080 -E production
 worker: bundle exec sidekiq -c 20
--- config/unicorn.rb
+++ config/unicorn.rb
@@ -3,9 +3,6 @@ timeout 15
 preload_app true
 
 before_fork do |server, worker|
-
-  @sidekiq_pid ||= spawn("bundle exec sidekiq -c 2")
-
   Signal.trap 'TERM' do
     puts 'Unicorn master intercepting TERM and sending myself QUIT instead'
     Process.kill 'QUIT', Process.pid
@@ -16,14 +13,6 @@ before_fork do |server, worker|
 end
 
 after_fork do |server, worker|
-
-  Sidekiq.configure_client do |config|
-    config.redis = { :size => 1 }
-  end
-  Sidekiq.configure_server do |config|
-    config.redis = { :size => 5 }
-  end
-
   Signal.trap 'TERM' do
     puts 'Unicorn worker intercepting TERM and doing nothing. Wait for master to send QUIT'
   end
