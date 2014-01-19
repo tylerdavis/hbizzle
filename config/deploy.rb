c3ab0b6f670fc7552434f88f30e85dd9971bb73b
Tyler Davis <tyebud@gmail.com>
2014-01-18 20:29:23 -0800

    Cap stuff

--- config/deploy.rb
+++ config/deploy.rb
@@ -7,6 +7,9 @@ set :deploy_to, '/home/hbizzle/hbizzle.com'
 set :scm, :git
 
 set :rails_env, 'production'
+set :default_env, { rvm_bin_path: '~/.rvm/bin' } 
+
+SSHKit.config.command_map[:rake] = "#{fetch(:default_env)[:rvm_bin_path]}/rvm ruby-#{fetch(:rvm_ruby_version)} do bundle exec rake"
 
 # set :format, :pretty
 # set :log_level, :debug
