db97e1e7bdeeed25185722560623a247610a5c40
Tyler Davis <tyebud@gmail.com>
2014-02-09 02:06:18 -0800

    More automation tasks

--- fabfile.py
+++ fabfile.py
@@ -19,7 +19,7 @@ def deploy():
   __pull('hbizzle')
   __bundle_install()
   rake('db:migrate')
-  sudo('restart hbizzle', pty=False)
+  __restart('hbizzle')
 
 def fetch():
   rake('fetch')
@@ -27,13 +27,9 @@ def fetch():
 def rake(command):
   run('rake ' + command)
 
-def start():
-  __start('hbizzle')
-
-def stop():
-  __stop('hbizzle')
-
-def restart():
+def rollback():
+  run('git reset HEAD@{1}')
+  __bundle_install()
   __restart('hbizzle')
 
 def push(remote='origin', branch='master'):
@@ -50,10 +46,10 @@ def __pull(directory, repository='origin', branch='master'):
 
 # Upstart commands
 def __start(service):
-  sudo('start ' + service)
+  sudo('start ' + service, pty=False)
 
 def __stop(service):
-  sudo('stop ' + service)
+  sudo('stop ' + service, pty=False)
 
 def __restart(service):
-  sudo('restart ' + service)
+  sudo('restart ' + service, pty=False)
